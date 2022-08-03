f5957aa970632d79269c5d409f54264a794ded9b
nithinvisva <visvanic@gmail.com>
2022-08-03 12:47:31 +0530

    deploy changes_1

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { io } from "socket.io-client";
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -11,7 +12,7 @@ export class ChatService {
   public message$= new BehaviorSubject<{i:number,j:number,userId:string| null, value:string, boxId:number}>({} as {i:number,j:number,userId:string| null, value:string, boxId:number});
   constructor() {}
 
-  socket = io('http://localhost:3000');
+  socket = io(environment.url);
 
   public sendMessage(data: {i:number,j:number,userId:string| null, boxId:number}) {
     this.socket.emit('message', data);
